abefa91b3e2cfc0bb84400d105336c1f4b4b1dbe
Generic35 <ricklamontagne@gmail.com>
2018-04-10 14:16:52 -0400

    assignment2, task, showing an Action Dialog

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -8,6 +8,7 @@ import 'rxjs/add/operator/switchMap';
 import { FavoriteService } from '../services/favorite.service';
 import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 import { Toasty } from 'nativescript-toasty';
+import { action } from "ui/dialogs";
 
 @Component({
   selector: 'app-dishdetail',
@@ -51,11 +52,29 @@ export class DishdetailComponent implements OnInit {
     if (!this.favorite) {
       console.log('Adding to Favorites', this.dish.id);
       this.favorite = this.favoriteservice.addFavorite(this.dish.id);
-      const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
+      const toast = new Toasty("Added Dish " + this.dish.id, "short", "bottom");
       toast.show();
     }
   }
 
+  openActionDialog() {
+    let options = {
+      title: "Dish Detail Actions",
+      message: "Choose Your Action",
+      cancelButtonText: "Cancel",
+      actions: ["Add to Favorites", "Add Comment"]
+    };
+
+    action(options).then((result) => {
+      if(result === "Add to Favorites"){
+        this.addToFavorites();
+        console.log('adding to favorites')
+      } else {
+        console.log('not adding to favorites')
+      }
+      console.log(`results of action dialog selection ${result}`);
+    });
+  }
   goBack(): void {
     this.routerExtensions.back();
   }
